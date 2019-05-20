108bf7a5dcfd64275d737df2994494b302945005
Hentoo <45872428+Hentoo@users.noreply.github.com>
2019-05-20 11:02:04 +0200

    testar

--- iMatMini/src/iMat/Controller/iMatMainController.java
+++ iMatMini/src/iMat/Controller/iMatMainController.java
@@ -1,16 +1,22 @@
 package iMat.Controller;
 
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.text.Text;
 import se.chalmers.cse.dat216.project.*;
 
+
 import java.net.URL;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.ResourceBundle;
+import java.sql.SQLOutput;
+import java.util.*;
 
 
 public class iMatMainController implements Initializable {
@@ -21,10 +27,54 @@ public class iMatMainController implements Initializable {
 
     @FXML
     private FlowPane productsFlowPane;
+    @FXML private Button categoryButton1;
+    @FXML
+    private ImageView imageField;
+    @FXML
+    Text productText;
+    @FXML
+    private TextField searchField;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     Product product = dataHandler.getProduct(45);
+    Product test = dataHandler.getProduct(30);
 
+
+    List<Product> dairyProducts = new ArrayList<>();
+
+
+    void populateFlowPane(Product product){
+        productText.setText(product.getName());
+
+
+    }
+
+    @FXML
+  private void pressedOnCategory1(){
+
+        productsFlowPane.getChildren().clear();
+
+      productsFlowPane.getChildren().add(new iMatProduct(test));
+
+  }
+/*
+
+*/
     private void updateProductList(List<Product> products) {
+
 
         productsFlowPane.getChildren().clear();
 
@@ -43,10 +93,36 @@ public class iMatMainController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
 
+
+
+/*
+        categoryButton1.cancelButtonProperty().addListener(new ChangeListener<Button>() {
+
+            @Override
+            public void changed(ObservableValue<? extends Button> observable, Button oldValue, Button newValue) {
+                populateFlowPane(test);
+             //   updateRecipeList();
+            }
+        });
+        */
+
         updateProductList(dataHandler.getProducts());
+
+
+
+
 
     }
 
+
+    @FXML
+    private void handleSearchAction(ActionEvent event) {
+
+        List<Product> matches = dataHandler.findProducts(searchField.getText());
+        updateProductList(matches);
+        System.out.println("# matching products: " + matches.size());
+
+    }
     private void updateCreditCard() {
 
      //   CreditCard card = model.getCreditCard();
