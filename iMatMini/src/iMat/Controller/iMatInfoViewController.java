2ec7cec3ef1fa534f8bc176d0f72cab9cb5872d3
klauzr <pontus.toender@gmail.com>
2019-05-22 21:55:51 +0200

    fixade köpprocessen på infoveiwn

--- iMatMini/src/iMat/Controller/iMatInfoViewController.java
+++ iMatMini/src/iMat/Controller/iMatInfoViewController.java
@@ -5,9 +5,12 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import se.chalmers.cse.dat216.project.IMatDataHandler;
 import se.chalmers.cse.dat216.project.Product;
+import se.chalmers.cse.dat216.project.ShoppingItem;
 
 import java.io.IOException;
 
@@ -21,6 +24,17 @@ public class iMatInfoViewController extends AnchorPane {
     private Label infoViewProductName;
     @FXML
     private Button closeInfoButton;
+    @FXML
+    private Button lessButton;
+    @FXML
+    private Button moreButton;
+    @FXML
+    private TextField amountField;
+    @FXML
+    private Button addToCart;
+
+    int chosenAmount = 1;
+
 
     private Product product;
 
@@ -44,6 +58,7 @@ public class iMatInfoViewController extends AnchorPane {
         this.infoViewPicture.setImage(model.getImage(product));
         this.infoViewProductName.setText(product.getName());
         this.controller = controller;
+        updateAmountBar();
 
 
 
@@ -53,6 +68,70 @@ public class iMatInfoViewController extends AnchorPane {
     @FXML
     private void closeInfoView(){
         controller.mainToFront();
+    }
+
+    @FXML
+    private void lesserButtonAction(){
+        if(chosenAmount > 1){
+            amountField.clear();
+            chosenAmount--;
+            updateAmountBar();
+        }
+
+    }
+
+    @FXML
+    private void moreButtonAction(){
+        amountField.clear();
+        chosenAmount++;
+        updateAmountBar();
+    }
+
+    private void updateAmountBar(){
+        amountField.clear();
+        amountField.appendText(Integer.toString(chosenAmount));
+    }
+
+
+    @FXML
+    private void setAddToCartAction() {
+
+        controller.shoppingCartArea.clear();
+        double totalPrice = 0;
+
+        int i = 0;
+        ShoppingItem checkCopy = null;
+
+        for (int j = 0; j < IMatDataHandler.getInstance().getShoppingCart().getItems().size(); j++) {
+            if (IMatDataHandler.getInstance().getShoppingCart().getItems().get(j).getProduct().getName() == product.getName()) {
+                checkCopy = IMatDataHandler.getInstance().getShoppingCart().getItems().get(j);
+                i = j;
+            }
+        }
+
+        if (i == 0 && checkCopy == null) {
+            IMatDataHandler.getInstance().getShoppingCart().getItems().add(new ShoppingItem(product, chosenAmount));
+        }
+
+        if (checkCopy != null) {
+            IMatDataHandler.getInstance().getShoppingCart().getItems().add(new ShoppingItem(checkCopy.getProduct(), checkCopy.getAmount() + chosenAmount));
+            IMatDataHandler.getInstance().getShoppingCart().getItems().remove(i);
+        }
+
+
+        for (ShoppingItem shoppingItem : IMatDataHandler.getInstance().getShoppingCart().getItems()) {
+            controller.shoppingCartArea.appendText(shoppingItem.getProduct().getName() + "           " + (int) shoppingItem.getAmount() + " st." + "\n");
+        }
+
+        for (ShoppingItem shoppingItem : IMatDataHandler.getInstance().getShoppingCart().getItems()) {
+            totalPrice = totalPrice + (shoppingItem.getProduct().getPrice() * shoppingItem.getAmount());
+        }
+        controller.totalPriceLabel.setText("TOTALPRIS: " + Integer.toString((int) totalPrice) + " SEK");
+
+        chosenAmount = 1;
+        updateAmountBar();
+        controller.mainToFront();
+
     }
 
 
