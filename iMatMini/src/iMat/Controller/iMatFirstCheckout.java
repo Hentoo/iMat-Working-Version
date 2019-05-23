181ab512051c6fccbc9d848b346f38ab431d26c9
Hentoo <45872428+Hentoo@users.noreply.github.com>
2019-05-23 16:52:37 +0200

    ss

--- iMatMini/src/iMat/Controller/iMatFirstCheckout.java
+++ iMatMini/src/iMat/Controller/iMatFirstCheckout.java
@@ -1,13 +1,19 @@
 package iMat.Controller;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.FlowPane;
 import se.chalmers.cse.dat216.project.Product;
 
 import java.io.IOException;
 
 public class iMatFirstCheckout extends AnchorPane {
 
+
+
+    @FXML
+    FlowPane firstCheckoutFlowPane;
 
     public iMatFirstCheckout() {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("iMatFirstCheckout.fxml"));
